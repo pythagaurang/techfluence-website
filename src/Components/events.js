2716e93ef26583ecc4c046f9733a996fd370fc10
moriarty128 <gaurangsg.28@gmail.com>
2019-09-30 12:49:49 +0530

    boxes

--- src/Components/events.js
+++ src/Components/events.js
@@ -9,8 +9,9 @@ class events extends Component {
     console.log(this.props)
     const event = this.props.event ? (
       <div className="event center-align container">
-        <h3 className="center-align"><b>{this.props.event.title}</b></h3>
+        <h2 className="center-align"><b>{this.props.event.title}</b></h2>
         {this.props.event.collab  ? (<h6>In collaboration with {this.props.event.collab}</h6>):(<p/>)}
+        <div className="details container">
         <h4 className="center-align">Details</h4>
         {this.props.event.day  ? (<div><p><b>Date</b></p><p>{this.props.event.day}</p></div>):(<p/>)}
         {this.props.event.time  ? (<div><p><b>Time</b></p><p>{this.props.event.time[0]}</p><p>{this.props.event.time[1]}</p><p>{this.props.event.time[2]}</p></div>):(<p></p>)}
@@ -18,8 +19,11 @@ class events extends Component {
         {this.props.event.team  ? (<div><p><b>Team</b></p><p>{this.props.event.team}</p></div>):(<p/>)}
         {this.props.event.coordinator  ? (<div><p><b>Co-ordinators</b></p><p>{this.props.event.coordinator[0]}</p><p>{this.props.event.coordinator[1]}</p><p>{this.props.event.coordinator[2]}</p></div>):(<p/>)}
         {this.props.event.faculty  ? (<div><p><b>Faculty Co-ordinators</b></p><p>{this.props.event.faculty[0]}</p><p>{this.props.event.faculty[1]}</p><p>{this.props.event.faculty[2]}</p></div>):(<p/>)}
+        </div>
+        <div className="instructions container">
         <h4 className="center-align">Instructions</h4>
         <Eventlist rules={this.props.event.rules} />
+        </div>
       </div>
     ) : (
       <div className="center">Loading event...</div>
