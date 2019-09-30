74fffa511723bca314f42f9a36f7f009b4d0b728
moriarty128 <gaurangsg.28@gmail.com>
2019-09-30 15:20:49 +0530

    copyright

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -27,7 +27,7 @@ class Footer extends Component{
         </div>
         <div className="footer-copyright">
           <div className="container">
-          © 2019 TechFluence
+          © 2019 Gaurang Govekar
           <a className="grey-text text-lighten-4 right" href="https://github.com/pythagaurang/techfluence-website/"><i className="fab fa-github"></i> Github</a>
           </div>
         </div>
